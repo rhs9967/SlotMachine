0e9461b2a686d6b1f28be3a62f54db0978f16c42
Carl Milazzo <cxm7805@rit.edu>
2014-05-06 13:45:41 -0400

    Started a reel class

---
+++ Slot Machine/Reel.m
@@ -0,0 +1,13 @@
+//
+//  Reel.m
+//  Slot Machine
+//
+//  Created by Carl Milazzo on 5/6/14.
+//  Copyright (c) 2014 Carl Milazzo and Bob Schrupp. All rights reserved.
+//
+
+#import "Reel.h"
+
+@implementation Reel
+
+@end
