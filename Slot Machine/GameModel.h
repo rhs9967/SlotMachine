d11aeaabdb168868624b9e51b743787508004282
Robert Schrupp <schruppr@comcast.net>
2014-05-06 14:16:27 -0400

    Added GameModel class

---
+++ Slot Machine/GameModel.h
@@ -0,0 +1,13 @@
+//
+//  GameModel.h
+//  Slot Machine
+//
+//  Created by Student on 5/6/14.
+//  Copyright (c) 2014 Carl Milazzo and Bob Schrupp. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameModel : SKNode
+
+@end
---
+++ Slot Machine/GameModel.m
@@ -0,0 +1,13 @@
+//
+//  GameModel.m
+//  Slot Machine
+//
+//  Created by Student on 5/6/14.
+//  Copyright (c) 2014 Carl Milazzo and Bob Schrupp. All rights reserved.
+//
+
+#import "GameModel.h"
+
+@implementation GameModel
+
+@end
